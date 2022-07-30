7de3f8751172600bf46ce90bf0d1c7f47a9da6fb
pxai <pello.altadill@gluckgames.com>
2022-07-30 11:49:27 +0200

    Added redux-persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,9 @@
 import { compose, createStore, applyMiddleware } from 'redux';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 // import logger from 'redux-logger';
-import { rootReducer } from './root-reducer';
-
 //const middlewares = [logger]; // TODO, just dev
+import { rootReducer } from './root-reducer';
 
 const loggerMiddleware = (store) => (next) => (action) => {
   if (!action.type) {
@@ -20,6 +21,20 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
 const middleWares = [loggerMiddleware];
 
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['user'],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
