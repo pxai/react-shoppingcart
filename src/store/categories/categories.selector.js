09327df6b5319bee30371efc29656a08ca48afbe
pxai <pello.altadill@gluckgames.com>
2022-07-30 10:49:14 +0200

    added reselect for category reducer

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,11 +1,18 @@
-export const selectCategoriesMap = state => {
-    const categoriesMap = state.categories.categories.reduce(
-            (acc, {title, items}) => {
-            acc[title.toLowerCase()] = items;
-            return acc;
-        }, 
-        {}
-    );
+import { createSelector } from 'reselect';
 
-    return categoriesMap;
-}
+const selectCategoryReducer = (state) => state.categories;
+
+export const selectCategories = createSelector(
+  [selectCategoryReducer],
+  (categoriesSlice) => categoriesSlice.categories
+);
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories) =>
+    categories.reduce((acc, category) => {
+      const { title, items } = category;
+      acc[title.toLowerCase()] = items;
+      return acc;
+    }, {})
+);
